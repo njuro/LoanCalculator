c5ca1d9cfd3fd4a9c0e2070ec6931702a895307c
njuro <juraj.noge@gmail.com>
2018-05-19 08:38:04 +0200

    Initial commit

---
+++ server/src/main/java/eu/lundegaard/assignment/loancalculator/LoancalculatorApplication.java
@@ -0,0 +1,12 @@
+package eu.lundegaard.assignment.loancalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoancalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoancalculatorApplication.class, args);
+    }
+}
