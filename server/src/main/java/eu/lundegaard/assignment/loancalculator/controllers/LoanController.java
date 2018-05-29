6b7a6358ca668d689c1b853c14c2bc2faa0743dd
njuro <juraj.noge@gmail.com>
2018-05-29 13:05:46 +0200

    Allow Cross-Origin access

--- server/src/main/java/eu/lundegaard/assignment/loancalculator/controllers/LoanController.java
+++ server/src/main/java/eu/lundegaard/assignment/loancalculator/controllers/LoanController.java
@@ -3,15 +3,17 @@ package eu.lundegaard.assignment.loancalculator.controllers;
 import eu.lundegaard.assignment.loancalculator.models.CalculatedLoan;
 import eu.lundegaard.assignment.loancalculator.models.Loan;
 import eu.lundegaard.assignment.loancalculator.utils.LoanCalculator;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoanController {
 
     @PostMapping(value = "/calculate-loan")
-    public CalculatedLoan calculateLoan(@ModelAttribute Loan loan) {
+    @CrossOrigin(origins = "http://localhost:4200")
+    public CalculatedLoan calculateLoan(@RequestBody Loan loan) {
         return LoanCalculator.calculateLoan(loan);
     }
 
