aa652e0186ca1a50a4633ed54c5ea88e3f89322e
njuro <juraj.noge@gmail.com>
2018-05-29 14:01:30 +0200

    Logging server response

--- client/src/app/loan.service.ts
+++ client/src/app/loan.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Loan} from './loan';
 import {Observable} from 'rxjs';
 import {CalculatedLoan} from './calculated-loan';
+import {tap} from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -20,10 +21,15 @@ export class LoanService {
   }
 
   calculateLoan(loan: Loan): Observable<CalculatedLoan> {
-    console.log('Posting loan');
+    console.log('Calculating loan...');
     console.log(loan);
     if (loan.numberOfMonths > 0 && loan.amount > 0) {
-      return this.http.post<CalculatedLoan>(this.apiUrl, loan, httpOptions);
+      return this.http.post<CalculatedLoan>(this.apiUrl, loan, httpOptions).pipe(
+        tap(l => {
+          console.log('Received calculated loan');
+          console.log(l);
+        })
+      );
     }
   }
 }
